ff7d1093fd4a13c98da0b2f085d690daa2fb8157
ckaylor33 <kaylor33@hotmail.co.uk>
2020-11-11 21:07:05 +0000

    adding alternate pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = [];
+let workItems = [];
 
 app.set(`view engine`, `ejs`);
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,13 +23,32 @@ app.get(`/`, (req, res) => {
 
   let day = today.toLocaleDateString(`en-US`, options);
 
-  res.render("list", { day: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect(`/`);
+  if (req.body.list === `Work`) {
+    workItems.push(item);
+    res.redirect(`work`);
+  } else {
+    items.push(item);
+    res.redirect(`/`);
+  }
+});
+
+app.get(`/work`, (req, res) => {
+  res.render(`list`, { listTitle: `Work List`, newListItems: workItems });
+});
+
+app.post(`/work`, (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect(`/work`);
+});
+
+app.get(`/about`, (req, res) => {
+  res.render(`about`);
 });
 
 app.listen(3000, () => {
